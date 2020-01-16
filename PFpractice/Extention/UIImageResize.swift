5f553d3d7bcc5da9001701d977b6004628a7c78a
Tshi66 <general.eva10@gmail.com>
2020-01-17 05:16:09 +0900

    画像を保存する際、データサイズを縮小させる。

---
+++ PFpractice/Extention/UIImageResize.swift
@@ -0,0 +1,25 @@
+//
+//  UIImageResize.swift
+//  PFpractice
+//
+//  Created by 渡辺崇博 on 2020/01/16.
+//  Copyright © 2020 渡辺崇博. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resized(withPercentage percentage: CGFloat) -> UIImage? {
+        let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+        return UIGraphicsImageRenderer(size: canvas, format: imageRendererFormat).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+    func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        return UIGraphicsImageRenderer(size: canvas, format: imageRendererFormat).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+}
