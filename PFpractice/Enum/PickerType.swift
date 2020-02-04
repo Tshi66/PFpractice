a1f0addaf16c31c607338b4f7acc9d2620f4c1aa
Tshi66 <general.eva10@gmail.com>
2020-02-04 13:56:57 +0900

    #7 通知設定画面のリファクタリング完了

---
+++ PFpractice/Enum/PickerType.swift
@@ -0,0 +1,14 @@
+//
+//  PickerType.swift
+//  PFpractice
+//
+//  Created by 渡辺崇博 on 2020/02/04.
+//  Copyright © 2020 渡辺崇博. All rights reserved.
+//
+
+import Foundation
+
+enum pickerType {
+    case time
+    case date
+}
